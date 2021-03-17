b8135dbe9095f86815739af39c2885a89a79cd76
K-Artisan <836439285@qq.com>
2021-03-17 21:32:11 +0800

    app ico

--- src/apb/src/apps/Zo.Xapp.Domain/Users/User.cs
+++ src/apb/src/apps/Zo.Xapp.Domain/Users/User.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp;
 using Volo.Abp.Domain.Entities.Auditing;
 
 namespace Zo.Xapp.Users
@@ -14,7 +16,48 @@ namespace Zo.Xapp.Users
         public string Phone { get; set; }
         public string Email { get; set; }
 
+        public DateTime BirthDate { get; set; }
+
         //public virtual List<UserFingerprint> Fingerprints { get; set; }
 
+        private User()
+        {
+            /* This constructor is for deserialization / ORM purpose */
+        }
+
+        internal User(
+            Guid id,
+            [NotNull] string userName,
+            [NotNull] string name)
+            : base(id)
+        {
+            SetUserName(userName);
+            SetName(name);
+
+        }
+
+        private void SetUserName([NotNull] string name)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                name,
+                nameof(name),
+                maxLength: UserConsts.MaxUserNameLength
+            );
+        }
+
+        internal User ChangeName([NotNull] string name)
+        {
+            SetName(name);
+            return this;
+        }
+
+        private void SetName([NotNull] string name)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                name,
+                nameof(name),
+                maxLength: UserConsts.MaxNameLength
+            );
+        }
     }
 }
---
+++ src/apb/src/apps/Zo.Xapp.Domain/Users/UserManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace Zo.Xapp.Users
+{
+    public class UserManager : DomainService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User> CreateAsync(
+            [NotNull] string userName,
+            [NotNull] string name)
+        {
+            Check.NotNullOrWhiteSpace(userName, nameof(userName));
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+            var existingUser = await _userRepository.FindByUserNameAsync(userName);
+            if (existingUser != null)
+            {
+                throw new UserFriendlyException(userName);
+            }
+
+            return new User(GuidGenerator.Create(), userName, name);
+        }
+    }
+}
